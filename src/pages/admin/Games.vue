1403947833af4790a6a9ceff1c1c3952327c07f2
voxace <dave@voxace.com>
2019-05-19 09:32:19 +1000

    New routes

---
+++ src/pages/admin/Games.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page>
+    <div class="row q-px-lg">
+      <div class="col-xs-12">
+        <h3>Games</h3>
+      </div>
+
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'Games',
+  meta: {
+    title: 'Games',
+    titleTemplate: title => `FMCAR - ${title}`,
+    meta: {
+      description: { name: 'FMCAR Games', content: 'Games' },
+      keywords: { name: 'fmcar forza xbox racing', content: 'FMCAR' },
+      equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
+    },
+  },
+};
+</script>
