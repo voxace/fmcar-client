c6000d8ebbc0391f8786d498794cbf65a67f4d86
voxace <dave@voxace.com>
2019-05-19 22:44:19 +1000

    Update AddGame.vue

--- src/components/Admin/AddGame.vue
+++ src/components/Admin/AddGame.vue
@@ -20,6 +20,13 @@
               :rules="[ val => val && val.length > 0 || 'Please enter a name']"
             />
           </div>
+
+          <!-- TODO
+            Show image if in editing mode
+            have a cross show up on hover
+            once the image is deleted, the uploader shows
+          -->
+
           <div class="col-xs-12">
             <q-uploader
               label="Game Image / Logo"
