a14f28a05d2667583f0966fb27f9f2747a512b3d
voxace <dave@voxace.com>
2019-05-27 15:53:52 +1000

    Add and edit tracks

---
+++ src/components/Admin/AddTrack.vue
@@ -0,0 +1,206 @@
+<template>
+  <q-dialog
+    v-model="visibility" persistent
+    style="width: 700px; max-width: 90vw;"
+  >
+    <q-card style="min-width: 400px">
+
+      <q-card-section>
+        <div class="text-h6">
+          {{mode}} Track
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="row">
+          <div class="col-xs-12">
+            <q-input
+              outlined v-model="addNewTrackModel.name"
+              label="Name" :dense="$q.screen.lt.sm"
+              :rules="[ val => val && val.length > 0 || 'Please enter a name']"
+            />
+          </div>
+          <div v-if="showImage" class="col-xs-12">
+            <q-img
+              class="rounded-borders cursor-pointer"
+              :src="getUrl"
+              :ratio="16/9"
+              @click="editingTrack.image = null"
+            >
+              <q-tooltip
+                class="round"
+                anchor="center middle"
+                self="center middle"
+                content-class="bg-transparent"
+              >
+                <q-btn round color="red" size="lg" icon="delete" />
+              </q-tooltip>
+            </q-img>
+          </div>
+
+          <div v-else class="col-xs-12">
+            <q-uploader
+              label="Track Image / Logo"
+              auto-upload flat bordered
+              accept=".jpg, image/*"
+              field-name="upload"
+              :url="uploadUrl"
+              style="width: 100%"
+              @uploaded="uploaded"
+            />
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-actions align="right" class="text-primary">
+        <q-btn
+          flat label="Cancel"
+          @click.stop="close"
+        />
+        <q-btn
+          flat label="Save"
+          text-color="green"
+          @click="save"
+          :disabled="!addTrackValidation"
+        />
+      </q-card-actions>
+
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+/* eslint-disable no-underscore-dangle */
+export default {
+  name: 'addTrackDialog',
+  props: {
+    editing: Boolean,
+    editingTrack: Object,
+    visibility: Boolean,
+  },
+  data() {
+    return {
+      uploadUrl: `${process.env.API}/upload`,
+      addExistingTrackModel: {
+        name: null,
+        image: null,
+        configurations: [''],
+        weatherOptions: [''],
+      },
+      addNewTrackModel: {
+        name: null,
+        image: null,
+        configurations: [''],
+        weatherOptions: [''],
+      },
+    };
+  },
+  mounted() {
+    if (this.editing === true) {
+      this.addNewTrackModel.name = this.editingTrack.name;
+      this.addNewTrackModel.image = this.editingTrack.image;
+      this.$forceUpdate();
+    }
+  },
+  methods: {
+
+    // Provides file name info on upload
+    uploaded(info) {
+      this.addNewTrackModel.image = info.xhr.response;
+    },
+
+    // Chooses between edit and create mode
+    async save() {
+      if (this.editing === true) {
+        await this.editTrack();
+      } else {
+        await this.addTrack();
+      }
+    },
+
+    // Creates a new track
+    async addTrack() {
+      await this.$axios
+        .post('/track', { model: this.addNewTrackModel })
+        .then(() => {
+          this.$q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'fas fa-check-circle',
+            message: 'Track created successfully!',
+          });
+          this.close();
+          this.$emit('trackAdded');
+        })
+        .catch((error) => {
+          console.log(`Error: ${error}`);
+          this.$q.notify({
+            color: 'red-4',
+            textColor: 'white',
+            icon: 'fas fa-cross-circle',
+            message: 'Error creating track!',
+          });
+        });
+    },
+
+    // Updates the track
+    async editTrack() {
+      await this.$axios
+        .patch(`/track/${this.editingTrack._id}`, { model: this.addNewTrackModel })
+        .then(() => {
+          this.$q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'fas fa-check-circle',
+            message: 'Track updated successfully!',
+          });
+          this.close();
+          this.$emit('trackAdded');
+        })
+        .catch((error) => {
+          console.log(`Error: ${error}`);
+          this.$q.notify({
+            color: 'red-4',
+            textColor: 'white',
+            icon: 'fas fa-cross-circle',
+            message: 'Error updating track!',
+          });
+        });
+    },
+
+    // Closes the dialog
+    close() {
+      this.$emit('close');
+    },
+  },
+  computed: {
+
+    // Validate form
+    addTrackValidation() {
+      return this.addNewTrackModel.name != null
+      && this.addNewTrackModel.name.length > 0;
+    },
+
+    // Choose between 'Add' or 'Editing' mode
+    mode() {
+      if (this.editing === true) {
+        return 'Edit';
+      }
+      return 'Add';
+    },
+
+    // Show image instead of uploader
+    showImage() {
+      if (this.editing === true && this.editingTrack.image != null) {
+        return true;
+      }
+      return false;
+    },
+
+    // Get image URL
+    getUrl() {
+      return `${process.env.BASE}/${this.editingTrack.image}`;
+    },
+  },
+};
+</script>
--- src/pages/admin/Tracks.vue
+++ src/pages/admin/Tracks.vue
@@ -4,8 +4,56 @@
       <div class="col-xs-12">
         <h3>Tracks</h3>
       </div>
-
+      <template v-for="track in loadedTracks">
+        <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 q-pa-sm" :key="track._id">
+          <q-card>
+            <q-img
+              v-if="track.image"
+              :src="getUrl(track.image)"
+              :ratio="16/9"
+            />
+            <q-img
+              v-else
+              src="statics/placeholder.png"
+              :ratio="16/9"
+            />
+            <q-card-section>
+              <div
+                class="text-center"
+                v-bind:class="{
+                  'text-subtitle1': $q.screen.gt.sm,
+                  'text-body2': $q.screen.lt.sm,
+                  }"
+              >
+                {{ track.name }}
+              </div>
+            </q-card-section>
+            <q-card-actions align="around">
+              <q-btn flat text-color="blue" @click="editTrack(track)">EDIT</q-btn>
+              <q-btn flat text-color="red" @click="deleteTrack(track._id)">DELETE</q-btn>
+            </q-card-actions>
+          </q-card>
+        </div>
+      </template>
     </div>
+    <!-- FLOATING BUTTON -->
+    <q-page-sticky v-if="editingAllowed" position="bottom-right" :offset="[18, 18]">
+      <q-btn
+        round
+        size="lg"
+        icon="add"
+        color="positive"
+        @click="addTrack"
+      >
+      </q-btn>
+    </q-page-sticky>
+    <!-- ADD TEAM DIALOG -->
+    <add-track-dialog
+      v-if="addTrackDialog"
+      :editing="editing" :editingTrack="editingTrack"
+      :visibility="addTrackDialog"
+      @close="addTrackDialog = false" @trackAdded="trackAdded"
+    />
   </q-page>
 </template>
 
@@ -22,6 +70,83 @@ export default {
       description: { name: 'FMCAR Tracks', content: 'Tracks' },
       keywords: { name: 'fmcar forza xbox racing', content: 'FMCAR' },
       equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
+    },
+  },
+  components: {
+    addTrackDialog: () => import('components/Admin/AddTrack.vue'),
+  },
+  data() {
+    return {
+      loadedTracks: [],
+      loadingTracks: false,
+      addTrackDialog: false,
+      editing: false,
+      editingTrack: {
+        name: null,
+        image: null,
+      },
+    };
+  },
+  mounted() {
+    this.loadTracksList();
+  },
+  methods: {
+    async loadTracksList() {
+      this.loadingTracks = true;
+      await this.$axios
+        .get('/track')
+        .then((response) => {
+          this.loadedTracks = response.data;
+        })
+        .catch((error) => {
+          console.log(`Error: ${error}`);
+        });
+      this.loadingTracks = false;
+    },
+    async addTrack() {
+      this.editing = false;
+      this.addTrackDialog = true;
+    },
+    async editTrack(track) {
+      this.editing = true;
+      this.editingTrack = track;
+      this.addTrackDialog = true;
+    },
+    async deleteTrack(id) {
+      await this.$axios
+        .delete(`/track/${id}`)
+        .then(() => {
+          this.$q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'fas fa-check-circle',
+            message: 'Track deleted successfully!',
+          });
+          this.loadTracksList();
+        })
+        .catch((error) => {
+          console.log(`Error: ${error}`);
+          this.$q.notify({
+            color: 'red-4',
+            textColor: 'white',
+            icon: 'fas fa-cross-circle',
+            message: 'Error deleting track!',
+          });
+        });
+    },
+    trackAdded() {
+      this.loadTracksList();
+    },
+    getUrl(image) {
+      return `${process.env.BASE}/${image}`;
+    },
+    close() {
+      this.$emit('close');
+    },
+  },
+  computed: {
+    editingAllowed() {
+      return this.$store.state.modules.editingAllowed;
     },
   },
 };
